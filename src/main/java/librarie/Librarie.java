75a617875b259839c2bbfb741a688524aba662a0
amadrid <amadridlar@gmail.com>
2017-09-18 11:45:32 +0200

    Clase Librarie con métodos addBook y clearBook

---
+++ src/main/java/librarie/Librarie.java
@@ -0,0 +1,29 @@
+package librarie;
+
+public class Librarie {
+	
+	//PROPERTIES
+	private String book;
+	private String NIF;
+	  
+	
+	//CONSTRUCTORS
+	public Librarie() {
+		
+	}
+	
+	
+	//METHODS
+	public boolean addBook (String bookName) {
+		 boolean result = false;
+		 
+		 return result;
+	}
+	
+	public boolean clearBook (String bookName) {
+		boolean result = false;
+		
+		return result;
+	}
+
+}
