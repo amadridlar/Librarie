356f7fa25920de825acbccc6f7d2de8e170b56ec
amadrid <amadridlar@gmail.com>
2017-11-03 17:02:27 +0100

    json request bdd

--- src/main/java/com/aml/runner/RunClass.java
+++ src/main/java/com/aml/runner/RunClass.java
@@ -1,17 +1,17 @@
 package com.aml.runner;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+
+import org.json.simple.JSONObject;
 
 import com.aml.librarie.Book;
-import com.aml.librarie.Librarie;
+import com.aml.librarie.request.RequestJson;
 
 
 public class RunClass {
 
 	public static void main(String[] args) throws IOException{
-		
+		/*
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		Book book;
 		Librarie librarie = new Librarie ();
@@ -20,7 +20,14 @@ public class RunClass {
 		String bookName = br.readLine();
 		book = new Book(011, bookName, "Johny Reland");
 		librarie.addBook(book);
-
+		*/
+		
+		Book book = new Book(112, "Kill'em all", "Johny Reland");
+		RequestJson post;
+		post = new RequestJson();
+		JSONObject json;
+		json = post.createBookJson(book);
+		post.postJson(json);
 	}
 
 }
